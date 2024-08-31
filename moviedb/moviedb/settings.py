4da934cc8eb78fe9983bd54e40f06f6cf6b31b44
Indrek Kuusk <indrek.kuusk@gmail.com>
2024-08-31 10:47:10 +0300

    01.06.2024 4

--- moviedb/moviedb/settings.py
+++ moviedb/moviedb/settings.py
@@ -103,6 +103,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# settings.py
+LOGIN_REDIRECT_URL = '/accounts/profile/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -122,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
