254021edd9bb16c23def13c839849f68b45affd9
Indrek Kuusk <indrek.kuusk@gmail.com>
2024-08-31 13:39:21 +0300

    01.06.2024 4

--- moviedb/accounts/models.py
+++ moviedb/accounts/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(blank=True)
-    location = models.CharField(max_length=100, blank=True)
-    birth_date = models.DateField(null=True, blank=True)
-
-    def __str__(self):
-        return self.user.username
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     bio = models.TextField(blank=True)
+#     location = models.CharField(max_length=100, blank=True)
+#     birth_date = models.DateField(null=True, blank=True)
+#
+#     def __str__(self):
+#         return self.user.username
 
 from django.db.models.signals import post_save
 from django.dispatch import receiver
